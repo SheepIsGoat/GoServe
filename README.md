0adecc8ff4159e789a1aa05eccdeda854e75dfe7
SheepIsGoat <funk.ml.engineering@gmail.com>
2023-09-19 13:59:13 -0700

    added working endpoints to server

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # GoServe
+# create your postgres instance
+`docker run --name my-shades-postgres -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres`
+`docker exec -it my-shades-postgres psql -U postgres`
+
+# Create your database and table
+```
+CREATE DATABASE server_db;
+\c server_db
+CREATE TABLE users (
+    username VARCHAR(32) PRIMARY KEY,
+    password VARCHAR(100)
+);
+CREATE TABLE content ( 
+    content_id SERIAL PRIMARY KEY, 
+    title VARCHAR(64), 
+    summary VARCHAR(256), 
+    background_url VARCHAR(128),
+    datetime TIMESTAMP
+);
+INSERT INTO content (
+        content_id,
+        title,
+        summary,
+        background_url,
+        datetime
+    ) VALUES (
+        1,
+        'Dogs strike back',
+        'Are dogs bad, or just fluffy friends?',
+        'https://amazon.s3.us-east-1.120-48fj-Gjf',
+        '2023-09-19 01:50:00'
+    )
+```
+
+# Create your user
+`curl -X POST -d "username=gopher&password=G0ph3r" http://localhost:8080/create_user`
+
+# Login as your user
+`curl -X POST -d "username=gopher&password=G0ph3r" http://localhost:8080/login`
+
+# Request content
+`curl -H "Content-Type: application/json" -H "Authorization: Bearer YOUR_TOKEN_HERE" -d "{\"time\":\"2023-09-18T06:36:00\"}" http://localhost:8080/new_content_p`
--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"errors"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -21,16 +22,16 @@ type PoolConfig struct {
 	ConnectTimeout   time.Duration
 }
 
-func GetDefaultConfig() *PoolConfig {
+func getDefaultConfig() *PoolConfig {
 	return &PoolConfig{
-		ConnectionString: "postgresql://postgres:mysecretpassword@localhost:5432/postgres",
+		ConnectionString: "postgresql://postgres:mysecretpassword@localhost:5432/server_db",
 		MaxConns:         10,
 		MinConns:         5,
 		ConnectTimeout:   time.Second * 5,
 	}
 }
 
-func GetConnectionPool(config *PoolConfig) (*pgxpool.Pool, error) {
+func getConnectionPool(config *PoolConfig) (*pgxpool.Pool, error) {
 	pgxConfig, err := pgxpool.ParseConfig(config.ConnectionString)
 	if err != nil {
 		return nil, err
@@ -46,6 +47,28 @@ func GetConnectionPool(config *PoolConfig) (*pgxpool.Pool, error) {
 	}
 
 	return pool, nil
+}
+
+func createUser(c echo.Context, pool *pgxpool.Pool) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Printf("Failed to hash password for username %v", username)
+		return err
+	}
+
+	encodedPassHash := base64.StdEncoding.EncodeToString(passHash)
+
+	sqlStatement := `INSERT INTO users (username, password) VALUES ($1, $2)`
+
+    // Execute the SQL statement with the desired values.
+    _, err = pool.Exec(context.Background(), sqlStatement, username, encodedPassHash)
+	if err != nil {
+		log.Printf("Failed to create user with username %v. Error: %v", username, err)
+	}
+	return err
 }
 
 func loginEndpoint(c echo.Context, pool *pgxpool.Pool) error {
@@ -86,6 +109,90 @@ func loginEndpoint(c echo.Context, pool *pgxpool.Pool) error {
 	})
 }
 
+type ParentContentStruct struct {
+	Username string
+	NewContentStruct
+}
+
+func content(c echo.Context, pool *pgxpool.Pool, contentINF ParentContentStruct, contentFunc func (*pgxpool.Pool, string, ParentContentStruct) ([]map[string]string, error)) (error) {
+    unJwt, ok := c.Get("user").(*jwt.Token)
+    if !ok {
+        return c.String(http.StatusUnauthorized, "Invalid token")
+    }
+
+    claims, ok := unJwt.Claims.(jwt.MapClaims)
+    if !ok {
+        return c.String(http.StatusUnauthorized, "Invalid claims format")
+    }
+
+    username, ok := claims["username"].(string)
+    if !ok {
+        return c.String(http.StatusUnauthorized, "Name not found in token")
+    }
+
+	// type jsonData struct {
+	// 	time string
+	// }
+	// var content jsonData
+	if err := c.Bind(&contentINF); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	// timeStr := content.time
+
+	// input := map[string]string{"username": username, "time": timeStr}
+	content, err := contentFunc(pool, username, contentINF)
+	if err != nil {
+		return c.String(500, err.Error())
+	}
+    return c.JSON(http.StatusOK, content)
+}
+
+type NewContentStruct struct {
+	Time string
+}
+
+func newContent(pool *pgxpool.Pool, username string, timeStruct ParentContentStruct) ([]map[string]string, error) {
+	
+	
+	timeStr := timeStruct.NewContentStruct.Time
+	if timeStr == "" {
+		return nil, errors.New("Time string is empty")
+	}
+	
+	timeFormat := "2006-01-02T15:04:05"
+	timeObj, err := time.Parse(timeFormat, timeStr)
+	if err != nil {
+		return nil, errors.New("Could not parse time " + timeStr)
+	}
+
+	query := "SELECT title, summary, background_url FROM content WHERE datetime>$1"
+	rows, err := pool.Query(context.Background(), query, timeObj)
+	if err != nil {
+		return nil, errors.New("Query failed: " + err.Error())
+	}
+	defer rows.Close()
+
+
+	var result []map[string]string
+	for rows.Next() {
+		var title, summary, background string
+		err := rows.Scan(&title, &summary, &background)
+		if err != nil {
+			log.Fatalf("Row scan failed: %v\n", err)
+		}
+		row := map[string]string {
+			"title": title,
+			"summary": summary,
+			"background": background,
+		}
+		
+		result = append(result, row)
+	}
+	
+	return result, nil
+}
+
+
 func accessible(c echo.Context) error {
 	return c.String(http.StatusOK, "Accessible")
 }
@@ -93,15 +200,15 @@ func accessible(c echo.Context) error {
 func restricted(c echo.Context) error {
 	user := c.Get("user").(*jwt.Token)
 	claims := user.Claims.(jwt.MapClaims)
-	name := claims["name"].(string)
-	return c.String(http.StatusOK, "Welcome "+name+"!")
+	username := claims["username"].(string)
+	return c.String(http.StatusOK, "Welcome "+username+"!")
 }
 
 func main() {
 	e := echo.New()
 
-	config := GetDefaultConfig()
-	pool, err := GetConnectionPool(config)
+	config := getDefaultConfig()
+	pool, err := getConnectionPool(config)
 	if err != nil {
 		log.Fatalf("Unable to create connection pool: %v", err)
 	}
@@ -111,11 +218,28 @@ func main() {
 		return loginEndpoint(c, pool)
 	})
 
+	e.POST("/create_user", func(c echo.Context) error {
+		return createUser(c, pool)
+	})
+
+	r := e.Group("/new_content")
+	r.Use(middleware.JWT([]byte("secret")))
+	r.GET("", func(c echo.Context) error{
+		return content(c, pool, ParentContentStruct{}, newContent)
+	})
+
+	ra := e.Group("/new_content_p")
+	ra.Use(middleware.JWT([]byte("secret")))
+	ra.POST("", func(c echo.Context) error{
+		return content(c, pool, ParentContentStruct{}, newContent)
+	})
+
+
 	e.GET("/", accessible)
 
-	r := e.Group("/restricted")
-	r.Use(middleware.JWT([]byte("secret")))
-	r.GET("", restricted)
+	// r := e.Group("/restricted")
+	// r.Use(middleware.JWT([]byte("secret")))
+	// r.GET("", restricted)
 
 	e.Start(":8080")
 }
